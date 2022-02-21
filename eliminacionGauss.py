ed6e1164c5057e31b7b3d5d49caae3bf25dd7bc1
Jose N <jose.nunez181930@potros.itson.edu.mx>
2022-02-21 14:45:24 -0700

    Add EliminacionGauss.py

---
+++ eliminacionGauss.py
@@ -0,0 +1,14 @@
+#
+# Metodo de eliminacion de Gauss
+#
+# Programa que pide el número de ecuaciones (máximo 10) y después los valores de los
+# coeficientes y término independiente para cada ecuación. El programa  calcula y
+# despliega los valores de la solución.
+#
+
+
+print('\n# Programa que pide el número de ecuaciones (máximo 10) y después los valores de los')
+print('# coeficientes y término independiente para cada ecuación. El programa  calcula y')
+print('# despliega los valores de la solución.\n')
+
+
