3d3561e06129b62b96ddac2980d239545376a230
Jose N <jose.nunez181930@potros.itson.edu.mx>
2022-02-21 21:24:39 -0700

    Se agrego la variable intependiente

--- eliminacionGauss/src/eliminacionGauss/EliminacionGauss.java
+++ eliminacionGauss/src/eliminacionGauss/EliminacionGauss.java
@@ -21,42 +21,47 @@ public class EliminacionGauss {
      */
     public static void main(String[] args) {
 
-        Integer ecuaciones[][] = leeEcuaciones();
+        Double ecuaciones[][] = leeEcuaciones();
 
-    
-        
         pivotea(ecuaciones);
-        
-            for (int i = 0; i < ecuaciones.length; i++) {
-            for (int j = 0; j < ecuaciones.length; j++) {
-              //  Arrays.sort(ecuaciones[i]);
+
+        for (int i = 0; i < ecuaciones.length; i++) {
+            for (int j = 0; j < ecuaciones[i].length; j++) {
+                //  Arrays.sort(ecuaciones[i]);
                 System.out.printf("[%s][%s]= %s ", i, j, ecuaciones[i][j]);
 
             }
             System.out.println("");
         }
         
+        eliminacionAdelante(ecuaciones);
 
         // TODO code application logic here
     }
 
-    public static Integer[][] leeEcuaciones() {
+    public static Double[][] leeEcuaciones() {
         Scanner entrada = new Scanner(System.in);
         System.out.println("Ingresa el numero de ecuaciones (Máximo 10): ");
         Integer tamanio = entrada.nextInt();
-
+        
         if (tamanio > 10) {
             System.out.println("");
             return null;
         } else {
-            Integer ecuaciones[][] = new Integer[tamanio][tamanio];
+            Double ecuaciones[][] = new Double[tamanio][tamanio + 1];
 
-            for (int i = 0; i < ecuaciones.length; i++) {
-                for (int j = 0; j < ecuaciones.length; j++) {
+            for (int i = 0; i < tamanio; i++) {
+                for (int j = 0; j < tamanio +1; j++) {
+                    if(j < tamanio){
+                        System.out.printf("ingresa el valor coeficiente [%s] de la ecuacion [%s]: ", j, i);
+                    ecuaciones[i][j] = entrada.nextDouble();
+                    }else{
+                        System.out.printf("ingresa el valor Independiente [%s] de la ecuacion [%s]: ", j, i);
 
-                    System.out.printf("ingresa el valor [%s] de la ecuacion [%s]: ", j, i);
+                    ecuaciones[i][j] = entrada.nextDouble();
+                    }
 
-                    ecuaciones[i][j] = entrada.nextInt();
+                    
 
                 }
             }
@@ -65,24 +70,49 @@ public class EliminacionGauss {
         }
     }
 
-    public static void pivotea(Integer[][] ecuaciones) {
+    public static void pivotea(Double[][] ecuaciones) {
         for (int i = 0; i < ecuaciones.length; i++) {
-            for (int j = 0; j < ecuaciones.length; j++) {
-                if(j+1< ecuaciones.length){
-                    if(ecuaciones[j][0] < ecuaciones[j+1][0]){
-                        Integer aux[]= ecuaciones[j];
-                        ecuaciones[j] = ecuaciones[j+1];
-                        ecuaciones[j +1] = aux;
+            for (int j = 0; j < ecuaciones[i].length ; j++) {
+                if (j + 1 < ecuaciones.length ) {
+                    
+                    if (ecuaciones[j][0] < ecuaciones[j + 1][0]) {
+                        Double aux[] = ecuaciones[j];
+                        ecuaciones[j] = ecuaciones[j + 1];
+                        ecuaciones[j + 1] = aux;
                     }
-                
+
+                }
             }
+
+        }
+
+    }
+    
+    public static void eliminacionGauss(){
+        
+    }
+    
+    
+    public static void eliminacionAdelante(Double[][] ecuaciones) {
+        Double pivote;
+        int contador = 0;
+        while (true) {
+            pivote = ecuaciones[contador][contador];
+                    
+            for (int i = 0; i < ecuaciones.length; i++) {
+                for (int j = 1; j < ecuaciones.length; j++) {
+                    if (i + 1 < ecuaciones.length) {
+                        pivote = (ecuaciones[i + 1][i]/ pivote);
+                        
+                    }
+                }
             }
             
+            contador++;
+            if(contador == ecuaciones.length-1){
+                break;
+            }
         }
-        
-       
-        
-        
 
     }
 
