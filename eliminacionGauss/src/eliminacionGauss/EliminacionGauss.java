72fa13ccff79c387417b5cd823af4a7618ba7934
Jose N <jose.nunez181930@potros.itson.edu.mx>
2022-02-22 16:04:28 -0700

    Eliminacion de Gauss

--- eliminacionGauss/src/eliminacionGauss/EliminacionGauss.java
+++ eliminacionGauss/src/eliminacionGauss/EliminacionGauss.java
@@ -5,76 +5,71 @@
  */
 package eliminacionGauss;
 
-import java.util.Arrays;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 /**
  *
- * @author el_fr
+ * @author Jose Alfredo Nuñez Aguirre 00000181930
  */
 public class EliminacionGauss {
 
-    Integer ecuaciones[][];
+    public static Double ecuaciones[][];
+    public static Double resultados[];
+    public static String format = "00.0000";
+    // Constante para dar formata la salida de los resultados
+    public static final DecimalFormat FORMATEADOR = new DecimalFormat(format);
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        ecuaciones = leeEcuaciones();
+        eliminacionGauss();
+        despliegaSolucion();
+    }
 
-        Double ecuaciones[][] = leeEcuaciones();
-
-        pivotea(ecuaciones);
-
-        for (int i = 0; i < ecuaciones.length; i++) {
-            for (int j = 0; j < ecuaciones[i].length; j++) {
-                //  Arrays.sort(ecuaciones[i]);
-                System.out.printf("[%s][%s]= %s ", i, j, ecuaciones[i][j]);
-
-            }
-            System.out.println("");
-        }
-        
-        eliminacionAdelante(ecuaciones);
-
-        // TODO code application logic here
+    public static void eliminacionGauss() {
+        pivotea();
+        eliminacionAdelante();
+        resultados = sustitucionAtras();
     }
 
     public static Double[][] leeEcuaciones() {
         Scanner entrada = new Scanner(System.in);
         System.out.println("Ingresa el numero de ecuaciones (Máximo 10): ");
         Integer tamanio = entrada.nextInt();
-        
+
         if (tamanio > 10) {
             System.out.println("");
+            entrada.close();
             return null;
         } else {
             Double ecuaciones[][] = new Double[tamanio][tamanio + 1];
 
             for (int i = 0; i < tamanio; i++) {
-                for (int j = 0; j < tamanio +1; j++) {
-                    if(j < tamanio){
-                        System.out.printf("ingresa el valor coeficiente [%s] de la ecuacion [%s]: ", j, i);
-                    ecuaciones[i][j] = entrada.nextDouble();
-                    }else{
+                for (int j = 0; j < tamanio + 1; j++) {
+                    if (j < tamanio) {
+                        System.out.printf("ingresa el valor coeficiente   [%s] de la ecuacion [%s]: ", j, i);
+                        ecuaciones[i][j] = entrada.nextDouble();
+                    } else {
                         System.out.printf("ingresa el valor Independiente [%s] de la ecuacion [%s]: ", j, i);
 
-                    ecuaciones[i][j] = entrada.nextDouble();
+                        ecuaciones[i][j] = entrada.nextDouble();
                     }
-
-                    
 
                 }
             }
-
+            entrada.close();
             return ecuaciones;
         }
     }
 
-    public static void pivotea(Double[][] ecuaciones) {
+    public static void pivotea() {
         for (int i = 0; i < ecuaciones.length; i++) {
-            for (int j = 0; j < ecuaciones[i].length ; j++) {
-                if (j + 1 < ecuaciones.length ) {
-                    
+            for (int j = 0; j < ecuaciones[i].length; j++) {
+                if (j + 1 < ecuaciones.length) {
+
                     if (ecuaciones[j][0] < ecuaciones[j + 1][0]) {
                         Double aux[] = ecuaciones[j];
                         ecuaciones[j] = ecuaciones[j + 1];
@@ -87,33 +82,54 @@ public class EliminacionGauss {
         }
 
     }
-    
-    public static void eliminacionGauss(){
-        
-    }
-    
-    
-    public static void eliminacionAdelante(Double[][] ecuaciones) {
-        Double pivote;
-        int contador = 0;
-        while (true) {
-            pivote = ecuaciones[contador][contador];
-                    
-            for (int i = 0; i < ecuaciones.length; i++) {
-                for (int j = 1; j < ecuaciones.length; j++) {
-                    if (i + 1 < ecuaciones.length) {
-                        pivote = (ecuaciones[i + 1][i]/ pivote);
-                        
-                    }
+
+    public static void eliminacionAdelante() {
+
+        int indice = ecuaciones.length;
+
+        for (int k = 0; k < indice; k++) {
+
+            for (int i = k + 1; i < indice; i++) {
+
+                double factor = ecuaciones[i][k] / ecuaciones[k][k];
+
+                for (int j = k; j <= indice; j++) {
+
+                    ecuaciones[i][j] = ecuaciones[i][j] - ecuaciones[k][j] * factor;
                 }
-            }
-            
-            contador++;
-            if(contador == ecuaciones.length-1){
-                break;
             }
         }
 
+    }
+
+    public static Double[] sustitucionAtras() {
+        int indice = ecuaciones.length;
+        Double[] resultado = new Double[indice];
+
+        resultado[indice - 1] = ecuaciones[indice - 1][indice]
+                / ecuaciones[indice - 1][indice - 1];
+
+        for (int f = indice - 2; f >= 0; f--) {
+
+            double sum = 0;
+
+            for (int i = indice - 1; i > f; i--) {
+
+                sum += ecuaciones[f][i] * resultado[i];
+
+            }
+
+            resultado[f] = (ecuaciones[f][indice] - sum) / ecuaciones[f][f];
+        }
+
+        return resultado;
+    }
+
+
+    public static void despliegaSolucion() {
+        for (int i = 0; i < resultados.length; i++) {
+            System.out.printf("X_%d=  %s\t", i + 1, FORMATEADOR.format(resultados[i]));
+        }
     }
 
 }
