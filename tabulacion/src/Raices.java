e79bfa45a51c07991d149423ae241e0badab313c
Jose N <jose.nunez181930@potros.itson.edu.mx>
2022-06-02 15:12:28 -0700

    Refactorizacion de funciones

--- tabulacion/src/Raices.java
+++ tabulacion/src/Raices.java
@@ -4,48 +4,64 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 
 public class Raices {
+    // variable inicial
+    public static double inicio;
 
-    public static void main(String[ ]args) throws Exception  {
+    //variable final
+    public static double fin;
 
-        // variable inicial
-        double inicio;
+    // variable incremento
+    public static double incremento;
 
-        //variable final
-        double fin;
+    public static void main(String[ ]args)   {
 
-        // variable incremento
-        double incremento;
 
-        // variable resultado
+
+
+        try {
+            // Objeto para recibir la entrada por consola
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
+            // Se ingresa el valor inicial y se guarda en la variable inicio
+            System.out.print("Valor inicial: ");
+            inicio = Double.parseDouble( entrada.readLine());
+
+            // Se ingresa el valor final y se guarda en la variable fin
+            System.out.print("Valor Final: ");
+            fin = Double.parseDouble(entrada.readLine());
+
+            // Se ingresa el valor del incremento
+            System.out.print("Valor de Incremento: ");
+            incremento = Double.parseDouble(entrada.readLine());
+
+            tabula(inicio, fin, incremento);
+        }catch (IOException e){
+            System.err.println("IOException: " + e.getMessage());
+        }catch (NumberFormatException e){
+            System.err.println("NumberFormat: "+e.getMessage());
+        }
+        finally {
+            System.out.println("\'-----------------------\'");
+        }
+    }
+
+
+    public static double f(double x){
+        // Se calcula x^3 + 2x^2 + 7 -20 y se devuelve el valor obtenido como un tipo double
+        return  Math.pow(x,3) + (2*Math.pow(x,2)) + (7*x) - 20;
+    }
+
+    public static void tabula(double inicio,double fin, double incremento){
         double resultado;
-
-
-        // Objeto para recibir la entrada por consola
-        BufferedReader entrada = new BufferedReader(
-                new InputStreamReader(System.in));
-
-        // Se ingresa el valor inicial y se guarda en la variable inicio
-        System.out.print("Valor inicial: ");
-        inicio = Double.valueOf( entrada.readLine());
-
-        // Se ingresa el valor final y se guarda en la variable fin
-        System.out.print("Valor Final: ");
-        fin = Double.valueOf( entrada.readLine());
-
-        // Se ingresa el valor del incremento
-        System.out.print("Valor de Incremento: ");
-        incremento = Double.valueOf( entrada.readLine());
 
         // Se imprime en consola la cabecera de la tabla.
         System.out.println("x    \t f(x)");
-
-
 
         //  mientras que inicio sea menorigual que fin el ciclo continua.
         while(inicio <= fin){
 
             // se calcula la ecuacion x^3 + 2x^2 + 7x - 20 y se guarda el valor en la variable resultado
-            resultado = ecuacion(inicio);
+            resultado = f(inicio);
 
             // se imprime el resultado
             System.out.printf("%.2f\t f(%.2f)= %.2f\n",inicio,inicio,resultado);
@@ -54,15 +70,8 @@ public class Raices {
             // y se guarda el nuevo valor.
             inicio+=incremento;
 
-
         }
-        System.out.println();
-    }
 
-
-    public static double ecuacion(double x){
-        // Se calcula x^3 + 2x^2 + 7 -20 y se devuelve el valor obtenido como un tipo double
-        return  Math.pow(x,3) + (2*Math.pow(x,2)) + (7*x) - 20;
     }
 
 
